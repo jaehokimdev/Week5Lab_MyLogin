080f2359f5468ac9cbe4c307618abc94b3003237
jaehokimdev <101899896+jaehokimdev@users.noreply.github.com>
2022-10-05 23:46:23 -0600

    created User.java and made session.

--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -16,6 +17,10 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+
+        
         getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
 
     }
@@ -27,5 +32,4 @@ public class LoginServlet extends HttpServlet {
         
     }
 
-  
 }
--- web/WEB-INF/home.jsp
+++ web/WEB-INF/home.jsp
@@ -9,9 +9,10 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Home page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Home page</h1>
+        Hello 
     </body>
 </html>
